6a7eca159e7d6bf58aefade811a31551fb60f543
Pinchez25 <thuamwangi001@gmail.com>
2025-06-06 15:18:30 +0300

    Renamed project to jenga-api-client

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import setup, find_packages  # noqa: H301
 #
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
-NAME = "jenga-client"
+NAME = "jenga-api-client"
 VERSION = "1.0.0"
 PYTHON_REQUIRES = ">= 3.9"
 REQUIRES = [
